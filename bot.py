39deee99f6603761327bcaf851efed9267fcfb26
warven428 <bbarwarii78@gmail.com>
2026-07-09 04:54:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,36 @@
 from telethon import TelegramClient, events
-import asyncio
 from telethon.sessions import StringSession
 
 # ========== ڕێکخستنەکان ==========
-api_id = 33790522
-api_hash = "00e4131295f55452e143c06099c1ddae"
-phone = "+96407504399022"  # ئەمە بگۆڕە بۆ ژمارەی مۆبایلی ڕاست
+api_id = 33774652
+api_hash = "c438941d8f43a0ff59fcc4b3f3c2fb42"
+session = "1AZWarzgBu1cI3bDK6Z5t5B_Nm883wvzXDR3WpiW2kcB11209myG8nE015D9JbVByYvFIipC5kQZ2ksQ7ZbytPyUDTTZEJE-80d_oJsKfq21GE7H8Iped8VbzIaTp8P9szTB1h4z6HFKRfE0HKJKUnNQ3g5P0LOdbM71My5njPM1jrF_UfgCi9n1yYE23DioUFM034xPbei2FrMEFgX_TJkvODci5V115KBuskijg9wK1voe1S8oM53wQp3Svn7ZCcJx_zar7113rBqBpdusahrCIwAAFad6RgQ6UhD9ARJDk0wH0JIMh651Lt6YahYJF5JM5CDGcMUVLAJnvRSPK4yoAjiVeALQ="
 # ===================================
 
 SOURCE_CHANNEL = "@WarnisxCcScrap"
-TARGET_CHANNEL = "@Cc428Kurd"
+TARGET_CHANNEL = "@Cc428Card"
 
-client = TelegramClient(StringSession(), api_id, api_hash)
+client = TelegramClient(StringSession(session), api_id, api_hash)
 
-async def main():
-    await client.start(phone)
-    print("✅ سەرکەوتووانە چووە ناوەوە!")
+@client.on(events.NewMessage(chats=SOURCE_CHANNEL))
+async def handler(event):
+    msg = event.message
 
-    async for message in client.iter_messages(SOURCE_CHANNEL, limit=1):
-        if message.text:
-            await client.send_message(TARGET_CHANNEL, message.text)
-            print(f"✅ پەیام کۆپی کرا بۆ {TARGET_CHANNEL}!")
-        elif message.photo:
-            await client.send_file(TARGET_CHANNEL, message.photo, caption=message.caption)
-            print(f"✅ وێنە کۆپی کرا بۆ {TARGET_CHANNEL}!")
+    if msg.media:
+        data = await msg.download_media(file=bytes)
+        await client.send_file(
+            TARGET_CHANNEL,
+            data,
+            caption=msg.text or "",
+            formatting_entities=msg.entities
+        )
+    else:
+        await client.send_message(
+            TARGET_CHANNEL,
+            msg.text or "",
+            formatting_entities=msg.entities
+        )
 
-asyncio.run(main())
+print("Bot is running...")
+client.start()
+client.run_until_disconnected()
