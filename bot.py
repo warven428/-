e86109c5b9864af76c8aea8b5a00025325b44f26
warven428 <bbarwarii78@gmail.com>
2026-07-09 05:42:26 +0300

    Update session string and message handler
    Updated session string for bot authentication and modified message handling to replace usernames.

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telethon.sessions import StringSession
 # ========== ڕێکخستنەکان ==========
 api_id = 33774652
 api_hash = "c438941d8f43a0ff59fcc4b3f3c2fb42"
-session = "1AZWarzgBu3CZjzNDevUPZd1UxUvWL-MYU6kS_m9a1mn-GsrXFxwI z3X9iov-8q02E91SCOzTAUP4zv_LfbGip13Mih5AE sunq1pxL27b4 MAjdi iBMyb1dju7_mQn4j1zhZjwJQ DDRh_z1B0ZNiaMZbBxD3SFkj rbaXZ0wyNyii cPY9OBe0vLpCth-mb0X5897pJXuN2i9YCbznNm NOZEg3Gz2T4EQhJT6pMXnL8pghczA2vg3jsoLuG6eOQJvnZOWzc_4 dSH26x5WnfIO1WciaHFy-Y_zUa1sbYWn47oeb_ZyOyCGHeCAGBgIC DXk8GGKY4HQtgwSuh19C6HanLOXiX7NeY="
+session = "1AZWarzgBu3CZjzNDevUPZd1UxUvWL-MYU6kS_m9a1mn-GsrXFxwIz3X9iov-8q02E91SCOzTAUP4zv_LfbGip13Mih5AEsunq1pxL27b4MAjdi iBMyb1dju7_mQn4j1zhZjwJQDDRh_z1B0ZNiaMZbBxD3SFkjrbaXZ0wyNyii cPY9OBe0vLpCth-mb0X5897pJXuN2i9YCbznNmNOZEg3Gz2T4EQhJT6pMXnL8pghczA2vg3jsoLuG6eOQJvnZOWzc_4dSH26x5WnfIO1WciaHFy-Y_zUa1sbYWn47oeb_ZyOyCGHeCAGBgICDXk8GGKY4HQtgwSuh19C6HanLOXiX7NeY="
 # ===================================
 
 SOURCE_CHANNEL = "@WarnisxCcScrap"
@@ -16,7 +16,6 @@ client = TelegramClient(StringSession(session), api_id, api_hash)
 async def handler(event):
     msg = event.message
 
-    # گۆڕینی ناوەکان
     new_text = msg.text or ""
     new_text = new_text.replace("@About_Warnisx", "@warven_24")
     new_text = new_text.replace("@Warnisx", "@warven_24")
