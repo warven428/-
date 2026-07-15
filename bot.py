b0ac5b0ee0392ebe72ab0185c765fadacb563678
warven428 <bbarwarii78@gmail.com>
2026-07-15 06:30:47 +0300

    Replace old session string with new one
    Updated the session string to a new value for authentication.

--- bot.py
+++ bot.py
@@ -6,11 +6,11 @@ from telethon.sessions import StringSession
 API_ID = 33790522
 API_HASH = "00e4131295f55452e143c06099c1ddae"
 
-# 🔴 سێشنە ڕاستەکە (بەبێ ناوی چەنڵ)
-SESSION_STRING = "1AZWarzgBu0uU3sU7X0yxSsfs3Zmc2-5axcQ-R_y2elHPIwTL3L00aRpWoVhvfZuKoYEqW8uYYJtYF374DjBtvM85wEakiNNKFPN_p8GcNVRecOdLA25hM6QtSilbZQ1WBhsXxRUL6HQKRTGsUtZPlofmGOFdcEuUg_1hsRSOftyjaELMU55cd6tEPOTeDvss94GQvOsV2EtiH8Mq1h9P5b3UvFPFKv0sUokYWUzrnzB1YanEzonF2gnCWWgcEiw8UGIMg0ZAUHlKox97OML0ipwOUP1dYOyeYEAKjRCrGeaN5NcMpennHlHHwkF707oTerI48DQVpF8SEGhAt6b4wrFFZUpMduA="
+# 🔴 سێشنە نوێیەکەت (گۆڕدرا بۆ ئەم سێشنە نوێیە)
+SESSION_STRING = "1AZWarzgBu24Baus9-uFQx-d_JbeuAGeajVqHfn5he3Ka6I4VS1LT1k3LPigjLzFXVe7GV1csKeCeN3dp3pWjRyrkIGI_Q8IZ12n6-RQUoHLb9Ybov1l2Rpff-qTR1yiBPs1OQjtQiGOr_K4gjBXTyAO6dpOA9kkJcUYpp-gaJKZI_iv9hz6gBmOopSFsq9LdvoTxCpj8_etlwJNsN10h1IarWJNPopEx_SEKC8pLJSQzgeaPkjyrkykNxlMmuJ3H_0LQZFg5OIHYpC5DASBW1m-fa6FDpRlTddi-7b3era05TvLjewIugzuKVHSsC0jKglS4T9GEjRG7_msr71UnVQBmAvt1y-U="
 
 SOURCE_CHANNEL = "@approved_card4"  # سەرچاوە
-TARGET_CHANNEL = "@Cc428Card"       # ئامانج (تەنیا لێرە دەبێت!)
+TARGET_CHANNEL = "@Cc428Card"       # ئامانج
 # ========================================================
 
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
