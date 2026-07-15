1dcc5ccf23db961c1157ea1cbc05594187fb39fc
warven428 <bbarwarii78@gmail.com>
2026-07-15 03:37:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 
-‎# ========== ڕێکخستنەکان ==========
+# ========== ڕێکخستنەکان ==========
+# !! ئاگاداری: تکایە api_id و api_hashـەکەت لە my.telegram.orgـەوە بە نوێ بکەوە !!
 api_id = 33774652
 api_hash = "c438941d8f43a0ff59fcc4b3f3c2fb42"
-session = "1AZWarzgBu3-KiH74R8lf-DsuIsRdxleaXi6S7pBPvKYH-SZLulIUltJTYR7WvTXR7Qgr04wYFVZVipBSn6FR8cZXa_oiJjmvskEtzYMr5W1CLF8hmYFyzqhq3ZM3rt_htKJLrpLuy5XWNMYoiBrPtSie9kfd1riyqcW1b34fTNhueoWZ4mcPGSeXdH1GaEMTG6URjaTBacCrEBlUzEexQEy5RMn6lUsdHLHrW30T79Mp5lNhK09aI9Mi0U0vtR7rnTXEUuVIXgyZxkkcgNDVS88eOcs24cCykxTyLdAsmvUXJJK0UY-G3p9xRnsQ06628-04aBuDm8aBRk4-ZDTrCclHxTN6Ojc="
+
+# سێشنە نوێیەکەت
+session = "1AZWarzgBu5kDz__vBJKG_yn1ldfgAd6cbqOCWEt1J7MJLwMxDzQ6TUYQ342FZAyPp4pfF0vD0L-JVGtsIqRhwzmCxj18l9m_eLqftF3F-_DYEDEFR4o7jdP82hjbAOQjx6qO5L50ZP9yYH3rKP0mhoNZfD98fPnqo9Hp81Qsw9L1bXVLkJKLbivSDPCz1vRtQMpUMytCaeDpuCbJeyxf5essFwqnYH6leNkT8VKak2E241SpV-l9DV2-0vpZFMPYlpUk6oau5UCExM6l_xaUNYTXNULg7HqdISO9JnC_kju5cmDg_6zDShAnJz_0_TEiF77bUbqP0yCC2j06esL6l-PnJNMIP1o="
 # ===================================
 
-SOURCE_CHANNEL = "@approved_card4"    # سەرچاوە (لێیەوە دەخوێنیتەوە)
-TARGET_CHANNEL = "@Cc428Card"         # ئامانج (بۆی دەنێریت) ← گۆڕدرا بۆ Cc428Card
+SOURCE_CHANNEL = "@approved_card4"    # سەرچاوە
+TARGET_CHANNEL = "@Cc428Card"         # ئامانج
 
 client = TelegramClient(StringSession(session), api_id, api_hash)
 
@@ -17,12 +20,11 @@ async def handler(event):
     msg = event.message
 
     new_text = msg.text or ""
-‎    # گۆڕینی هەموو ئاماژە کۆنەکان بۆ ناوی نوێ
     new_text = new_text.replace("@About_Warnisx", "@warven_24")
     new_text = new_text.replace("@Warnisx", "@warven_24")
     new_text = new_text.replace("@scrc1bot", "@warven_24")
 
-‎    # تەنیا تەکست دەنێرین (وێنە و میدیا فڕێ دەدرێن)
+    # تەنیا تەکست دەنێرین
     if new_text.strip() != "":
         await client.send_message(
             TARGET_CHANNEL,
@@ -30,6 +32,6 @@ async def handler(event):
             formatting_entities=msg.entities
         )
 
-print("Bot is running... (Media/Photos are IGNORED, Target is now @Cc428Card)")
+print("Bot is running... (Media/Photos are IGNORED)")
 client.start()
 client.run_until_disconnected()
